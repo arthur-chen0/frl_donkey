cae76cb01be655872bf466132b60b597b28c4d49
arthurchen <arthurchen@smail.nchu.edu.tw>
2023-08-17 22:14:13 +0800

    fixed type error

--- frl_server.py
+++ frl_server.py
@@ -8,15 +8,16 @@ def create_strategy() -> fl.server.strategy:
     strategy_name = train_config['settings']['aggregationFn']
     
     strategy = None
+    clients = int(train_config['settings']['clients'])
 
     if "FedAvg" in strategy_name:
         print("Server Strategy: FedAvg")
         strategy = fl.server.strategy.FedAvg(
             fraction_fit=0.5,
             fraction_evaluate=0.5,
-            min_fit_clients=train_config['settings']['clients'],
-            min_evaluate_clients=train_config['settings']['clients'],
-            min_available_clients=train_config['settings']['clients'])
+            min_fit_clients=clients,
+            min_evaluate_clients=clients,
+            min_available_clients=clients)
     
     elif "FedProx" in strategy_name:
         print("Server Strategy: FedProx")
@@ -24,15 +25,15 @@ def create_strategy() -> fl.server.strategy:
             fraction_fit=0.5,
             fraction_evaluate=0.5,
             proximal_mu=0.3,
-            min_fit_clients=train_config['settings']['clients'],
-            min_evaluate_clients=train_config['settings']['clients'],
-            min_available_clients=train_config['settings']['clients'])
+            min_fit_clients=clients,
+            min_evaluate_clients=clients,
+            min_available_clients=clients)
     
     if "dp" in strategy_name:
         print("Server Strategy with defferential privacy")
         return fl.server.strategy.DPFedAvgFixed(
             strategy, 
-            num_sampled_clients = train_config['settings']['clients'], 
+            num_sampled_clients = clients, 
             clip_norm = 0.001, 
             noise_multiplier = 0.001, 
             server_side_noising = True)
