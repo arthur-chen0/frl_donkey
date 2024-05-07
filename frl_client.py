6c40fde398e5333e7aab01ebf2fd3bc545b39494
Arthurchen <arthurchen@johnsonfitness.com>
2024-05-07 15:30:51 +0800

    Add state representation learning and Optuna to tune the hyperparameter

--- frl_client.py
+++ frl_client.py
@@ -3,26 +3,30 @@ import numpy as np
 import torch
 import configparser, argparse
 
-from model import DonkeyModel
+from common.model import DonkeyModel
 
 from collections import OrderedDict
 from typing import List, Tuple, Union
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--id",help="Client id")
-parser.add_argument("--env",help="Environment number")
+parser.add_argument("--id", help="Client id")
 args = parser.parse_args()
 
-train_config = configparser.ConfigParser()
+train_config = configparser.ConfigParser(allow_no_value=True)
 train_config.read('config.ini')
 
+
 class FlowerClient(fl.client.NumPyClient):
+
     def __init__(self):
-        self.donkeyModel = DonkeyModel(argEnvNum=args.env, carID=int(args.id))
+        self.donkeyModel = DonkeyModel(carID=int(args.id))
         self.model, self.env = self.donkeyModel.create()
 
     def get_parameters(self, config):
-        return [val.cpu().numpy() for _, val in self.model.policy.state_dict().items()]
+        return [
+            val.cpu().numpy()
+            for _, val in self.model.policy.state_dict().items()
+        ]
 
     def set_parameters(self, parameters):
         params_dict = zip(self.model.policy.state_dict().keys(), parameters)
@@ -33,28 +37,38 @@ class FlowerClient(fl.client.NumPyClient):
         print("start training...")
         # print("parameters: ", parameters[0])
         self.set_parameters(parameters)
+
         # set up model in learning mode with goal number of timesteps to complete
-        # self.model.learn(total_timesteps=10000)
         timesteps = int(train_config['RlSettings']['timesteps'])
         self.model.learn(total_timesteps=timesteps, reset_num_timesteps=False)
 
         self.obs = self.env.reset()
+
         print("start evalution after training...")
-        mean_reward, std_reward = self.evaluate_policy(eval_episodes_n=3, n_step=3000)
+        mean_reward, std_reward = self.evaluate_policy(eval_episodes_n=3,
+                                                       n_step=3000)
         self.model.logger.record("eval/mean_reward", mean_reward)
         self.model.logger.dump(step=self.model.num_timesteps)
 
-        print("done training... mean reward: ", mean_reward, "std: ", std_reward)
-        # print("parameters: ", self.get_parameters(config={})[0])
+        print("done training... mean reward: ", mean_reward, "std: ",
+              std_reward)
+
         self.model.save(self.donkeyModel.logdir + "_ppo_donkey")
-        # self.env.close()
-        
-        return self.get_parameters(config={}), int(train_config['RlSettings']['timesteps']), {}
+
+        return self.get_parameters(config={}), int(
+            train_config['RlSettings']['timesteps']), {}
 
     def evaluate(self, parameters, config):
         return 0.0, 1000, {"rewards": 0.0}
 
-    def evaluate_policy(self, eval_episodes_n: int = 10, n_step:int = 3000, deterministic: bool = True, render: bool = False, return_episode_rewards: bool = False,) -> Union[Tuple[float, float], Tuple[List[float], List[int]]]:
+    def evaluate_policy(
+        self,
+        eval_episodes_n: int = 10,
+        n_step: int = 3000,
+        deterministic: bool = True,
+        render: bool = False,
+        return_episode_rewards: bool = False,
+    ) -> Union[Tuple[float, float], Tuple[List[float], List[int]]]:
 
         episode_rewards = []
         curr_ep_reward = 0
@@ -75,16 +89,15 @@ class FlowerClient(fl.client.NumPyClient):
         print("episode rewards ", episode_rewards)
         mean_reward = np.mean(episode_rewards)
         std_reward = np.std(episode_rewards)
-        return mean_reward, std_reward     
-
+        return mean_reward, std_reward
 
 
 client = FlowerClient().to_client()
 
 # flwr.client.start_numpy_client() is deprecated. Instead, use `flwr.client.start_client()`
 fl.client.start_client(
-		server_address=train_config['FlSettings']['flwrServerIP'],
-		client=client,
-	)
+    server_address=train_config['FlSettings']['flwrServerIP'],
+    client=client,
+)
 
 client.numpy_client.env.close()
--- frl_server.py
+++ frl_server.py
@@ -1,12 +1,12 @@
 import flwr as fl
 import configparser
 
-from model import DonkeyModel
+from common.model import DonkeyModel
 from flwr.common import ndarrays_to_parameters
 
-
-train_config = configparser.ConfigParser()
+train_config = configparser.ConfigParser(allow_no_value=True)
 train_config.read('config.ini')
+
 
 def create_strategy() -> fl.server.strategy:
     strategy_name = train_config['FlSettings']['aggregationFn']
@@ -17,9 +17,9 @@ def create_strategy() -> fl.server.strategy:
     dp = train_config['FlSettings']['dp']
     clip_norm = float(train_config['FlSettings']['clip_norm'])
     noise_multiplier = float(train_config['FlSettings']['noise_multiplier'])
-    
+
     strategy = None
-    
+
     if "FedAvg" in strategy_name:
         print("Server Strategy: FedAvg")
         strategy = fl.server.strategy.FedAvg(
@@ -28,7 +28,7 @@ def create_strategy() -> fl.server.strategy:
             min_fit_clients=clients,
             min_evaluate_clients=clients,
             min_available_clients=clients)
-    
+
     elif "FedProx" in strategy_name:
         print("Server Strategy: FedProx")
         strategy = fl.server.strategy.FedProx(
@@ -38,34 +38,40 @@ def create_strategy() -> fl.server.strategy:
             min_fit_clients=clients,
             min_evaluate_clients=clients,
             min_available_clients=clients)
-        
+
     elif "FedYogi" in strategy_name:
         print("Server Strategy: FedYogi")
 
         model, env = DonkeyModel(argEnvNum=0).create()
-       
+
         strategy = fl.server.strategy.FedYogi(
             fraction_fit=fraction_fit,
             fraction_evaluate=fraction_evaluate,
             min_fit_clients=clients,
             min_evaluate_clients=clients,
             min_available_clients=clients,
-            initial_parameters=ndarrays_to_parameters([val.cpu().numpy() for _, val in model.policy.state_dict().items()]))
+            initial_parameters=ndarrays_to_parameters([
+                val.cpu().numpy()
+                for _, val in model.policy.state_dict().items()
+            ]))
         env.close()
 
     elif "FedAdam" in strategy_name:
         print("Server Strategy: FedAdam")
         model, env = DonkeyModel(argEnvNum=0).create()
-       
+
         strategy = fl.server.strategy.FedAdam(
             fraction_fit=fraction_fit,
             fraction_evaluate=fraction_evaluate,
             min_fit_clients=clients,
             min_evaluate_clients=clients,
             min_available_clients=clients,
-            initial_parameters=ndarrays_to_parameters([val.cpu().numpy() for _, val in model.policy.state_dict().items()]))
+            initial_parameters=ndarrays_to_parameters([
+                val.cpu().numpy()
+                for _, val in model.policy.state_dict().items()
+            ]))
         env.close()
-    
+
     elif "FedAdagrad" in strategy_name:
         print("Server Strategy: FedAdagrad")
         model, env = DonkeyModel(argEnvNum=0).create()
@@ -75,34 +81,38 @@ def create_strategy() -> fl.server.strategy:
             min_fit_clients=clients,
             min_evaluate_clients=clients,
             min_available_clients=clients,
-            initial_parameters=ndarrays_to_parameters([val.cpu().numpy() for _, val in model.policy.state_dict().items()]))
+            initial_parameters=ndarrays_to_parameters([
+                val.cpu().numpy()
+                for _, val in model.policy.state_dict().items()
+            ]))
         env.close()
-    
+
     if "dp_fixed_clipping" in dp:
         print("Server Strategy with defferential privacy: fixed clipping")
         return fl.server.strategy.DifferentialPrivacyServerSideFixedClipping(
-            strategy, 
-            num_sampled_clients = clients, 
-            clip_norm = clip_norm, 
-            noise_multiplier = noise_multiplier, 
-            server_side_noising = True)
+            strategy,
+            num_sampled_clients=clients,
+            clip_norm=clip_norm,
+            noise_multiplier=noise_multiplier,
+            server_side_noising=True)
+
     elif "dp_adaptive_clipping" in dp:
         print("Server Strategy with defferential privacy: adaptive clipping")
         return fl.server.strategy.DifferentialPrivacyServerSideAdaptiveClipping(
-            strategy, 
-            num_sampled_clients = clients, 
-            noise_multiplier = noise_multiplier,
+            strategy,
+            num_sampled_clients=clients,
+            noise_multiplier=noise_multiplier,
         )
     else:
         return strategy
-    
 
 
 def main() -> None:
     # Start Flower server
     fl.server.start_server(
         server_address="0.0.0.0:8080",
-        config=fl.server.ServerConfig(num_rounds=int(train_config['FlSettings']['rounds'])),
+        config=fl.server.ServerConfig(
+            num_rounds=int(train_config['FlSettings']['rounds'])),
         strategy=create_strategy(),
     )
 
